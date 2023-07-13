986d11e4a45800dd3cf9cb958e0a82ac1aa2b97d
fauzandp <fauzan27dp@gmail.com>
2023-07-13 17:11:48 +0700

    Project15_HWSwiftUI | Accessibility VoiceOver
    Introduction
    Identifying views with useful labels

---
+++ AccessibilityVoiceOver/AccessibilityVoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityVoiceOverApp.swift
+//  AccessibilityVoiceOver
+//
+//  Created by Fauzan Dwi Prasetyo on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityVoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
