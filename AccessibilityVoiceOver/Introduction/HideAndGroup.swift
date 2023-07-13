1eb2368ba529f8bf8cf1eeadef57129b27d7df9f
fauzandp <fauzan27dp@gmail.com>
2023-07-13 17:39:22 +0700

    Project15_HWSwiftUI | Accessibility VoiceOver
    Introduction
    Hiding and grouping accessibility data

---
+++ AccessibilityVoiceOver/Introduction/HideAndGroup.swift
@@ -0,0 +1,53 @@
+//
+//  HideAndGroup.swift
+//  AccessibilityVoiceOver
+//
+//  Created by Fauzan Dwi Prasetyo on 13/07/23.
+//
+
+import SwiftUI
+
+struct HideAndGroup: View {
+    var body: some View {
+            VStack {
+                Image(decorative: "character") // don't read image name
+                    .resizable()
+                    .scaledToFit()
+                // invisible from accessibility (useful for hidden elements)
+                    .accessibilityHidden(true)
+                
+                // will read “Your Score is” or “1000”
+                VStack {
+                    Text("Your Score is")
+                    Text("1000")
+                        .font(.title)
+                }
+                Spacer()
+                
+                // will read “Your Score is” delay then “1000” (pause between children)
+                VStack {
+                    Text("Your Score is")
+                    Text("1000")
+                        .font(.title)
+                }
+                .accessibilityElement(children: .combine)
+                Spacer()
+                
+                // will ignore children
+                // will read "Your Score is 1000"
+                VStack {
+                    Text("Your Score is")
+                    Text("1000")
+                        .font(.title)
+                }
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel("Your Score is 1000")
+            }
+    }
+}
+
+struct HideAndGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        HideAndGroup()
+    }
+}
